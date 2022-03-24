b2c947dbe2068175a5f9ed9dd66da1714bbe1f27
Aadarsh <75937477+aadarsh1911@users.noreply.github.com>
2022-03-24 18:44:12 +0530

    Add files via upload

---
+++ pet_shop-master/source code/welcome.php
@@ -0,0 +1,12 @@
+
+<?php
+
+
+
+// Grab User submitted information
+$email = $_POST["username"];
+$pass = $_POST["password"];
+
+if($email=='aadarsh'&& $pass=='ghorpade2001')
+echo "Connected successfully";
+?>
